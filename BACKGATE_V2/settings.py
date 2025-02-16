62a0e4ae3816dcf3ecc5f5d83c66eda3311dc302
Dipesh Acharya <dipeshacharya87@gmail.com>
2025-02-16 23:24:11 +0545

    prod setup 2

--- BACKGATE_V2/settings.py
+++ BACKGATE_V2/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-# import django
-# django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,8 +95,6 @@ CHANNEL_LAYERS = {
     }
 }
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
